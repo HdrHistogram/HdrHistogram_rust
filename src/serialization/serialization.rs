e26e30b538c3f88a7996e4ddf94ffffee7eb00de
Marshall Pierce <marshall+github@truevault.com>
2017-03-05 06:58:37 -0800

    LEB128-64b9B varint encoding

--- src/serialization/serialization.rs
+++ src/serialization/serialization.rs
@@ -1,7 +1,163 @@
+use std::io::{self, Read, Write};
+
 #[path = "tests.rs"]
 #[cfg(test)]
 mod tests;
 
+/// Write a number as a LEB128-64b9B little endian base 128 varint to buf. This is not
+/// quite the same as Protobuf's LEB128 as it encodes 64 bit values in a max of 9 bytes, not 10.
+/// The first 8 7-bit chunks are encoded normally (up through the first 7 bytes of input). The last
+/// byte is added to the buf as-is. This limits the input to 8 bytes, but that's all we need.
+#[allow(dead_code)]
+fn varint_write<W: Write> (input: u64, writer: &mut W) -> io::Result<usize> {
+    // The loop is unrolled because the special case is awkward to express in a loop, and it
+    // probably makes the branch predictor happier to do it this way.
+
+    let mut arr: [u8; 9] = [0; 9];
+    let mut bytes_used = 0;
+
+    if (input >> 7) == 0 {
+        arr[0] = input as u8;
+        bytes_used += 1;
+    } else {
+        // set high bit because more bytes are coming, then next 7 bits of value.
+        arr[0] = 0x80 | ((input & 0x7F) as u8);
+        bytes_used += 1;
+        if (input >> 7 * 2) == 0 {
+            // nothing above bottom 2 chunks, this is the last byte, so no high bit
+            arr[1] = nth_7b_chunk(input, 1);
+            bytes_used += 1;
+        } else {
+            arr[1] = nth_7b_chunk_with_high_bit(input, 1);
+            bytes_used += 1;
+            if (input >> 7 * 3) == 0 {
+                arr[2] = nth_7b_chunk(input, 2);
+                bytes_used += 1;
+            } else {
+                arr[2] = nth_7b_chunk_with_high_bit(input, 2);
+                bytes_used += 1;
+                if (input >> 7 * 4) == 0 {
+                    arr[3] = nth_7b_chunk(input, 3);
+                    bytes_used += 1;
+                } else {
+                    arr[3] = nth_7b_chunk_with_high_bit(input, 3);
+                    bytes_used += 1;
+                    if (input >> 7 * 5) == 0 {
+                        arr[4] = nth_7b_chunk(input, 4);
+                        bytes_used += 1;
+                    } else {
+                        arr[4] = nth_7b_chunk_with_high_bit(input, 4);
+                        bytes_used += 1;
+                        if (input >> 7 * 6) == 0 {
+                            arr[5] = nth_7b_chunk(input, 5);
+                            bytes_used += 1;
+                        } else {
+                            arr[5] = nth_7b_chunk_with_high_bit(input, 5);
+                            bytes_used += 1;
+                            if (input >> 7 * 7) == 0 {
+                                arr[6] = nth_7b_chunk(input, 6);
+                                bytes_used += 1;
+                            } else {
+                                arr[6] = nth_7b_chunk_with_high_bit(input, 6);
+                                bytes_used += 1;
+                                if (input >> 7 * 8) == 0 {
+                                    arr[7] = nth_7b_chunk(input, 7);
+                                    bytes_used += 1;
+                                } else {
+                                    arr[7] = nth_7b_chunk_with_high_bit(input, 7);
+                                    bytes_used += 1;
+                                    // special case: write last whole byte as is
+                                    arr[8] = (input >> 56) as u8;
+                                    bytes_used += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return writer.write_all(&arr[0..bytes_used]).map(|_| bytes_used)
+}
+
+/// Read a LEB128-64b9B from the buffer
+#[allow(dead_code)]
+fn varint_read<R: Read>(reader: &mut R) -> io::Result<u64> {
+    let mut b = read_u8(reader)?;
+
+    // take low 7 bits
+    let mut value: u64 = low_7_bits(b);
+
+    if is_high_bit_set(b) {
+        // high bit set, keep reading
+        b = read_u8(reader)?;
+        value |= low_7_bits(b) << 7;
+        if is_high_bit_set(b) {
+            b = read_u8(reader)?;
+            value |= low_7_bits(b) << 7 * 2;
+            if is_high_bit_set(b) {
+                b = read_u8(reader)?;
+                value |= low_7_bits(b) << 7 * 3;
+                if is_high_bit_set(b) {
+                    b = read_u8(reader)?;
+                    value |= low_7_bits(b) << 7 * 4;
+                    if is_high_bit_set(b) {
+                        b = read_u8(reader)?;
+                        value |= low_7_bits(b) << 7 * 5;
+                        if is_high_bit_set(b) {
+                            b = read_u8(reader)?;
+                            value |= low_7_bits(b) << 7 * 6;
+                            if is_high_bit_set(b) {
+                                b = read_u8(reader)?;
+                                value |= low_7_bits(b) << 7 * 7;
+                                if is_high_bit_set(b) {
+                                    b = read_u8(reader)?;
+                                    // special case: use last byte as is
+                                    value |= (b as u64) << 7 * 8;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(value)
+}
+
+/// input: a u64
+/// n: >0, how many 7-bit shifts to do
+/// Returns the n'th chunk (starting from least significant) of 7 bits as a byte with the the high
+/// bit unset.
+fn nth_7b_chunk(input: u64, n: u8) -> u8 {
+    (input >> 7 * n) as u8
+}
+
+/// input: a u64
+/// n: >0, how many 7-bit shifts to do
+/// Returns the n'th chunk (starting from least significant) of 7 bits as a byte.
+/// The high bit in the byte will be set (not one of the 7 bits that map to input bits).
+fn nth_7b_chunk_with_high_bit(input: u64, n: u8) -> u8 {
+    nth_7b_chunk(input, n) | 0x80
+}
+
+/// truncate byte to low 7 bits, cast to u64
+fn low_7_bits(b: u8) -> u64 {
+    (b & 0x7F) as u64
+}
+
+fn is_high_bit_set(b: u8) -> bool {
+    // TODO benchmark leading zeros rather than masking
+    (b & 0x80) != 0
+}
+
+fn read_u8<R: Read>(reader: &mut R) -> io::Result<u8> {
+    let mut buf = [0; 1];
+    reader.read_exact(&mut buf)?;
+    Ok(buf[0])
+}
 
 /// Map signed numbers to unsigned: 0 to 0, -1 to 1, 1 to 2, -2 to 3, etc
 #[allow(dead_code)] // TODO
--- src/serialization/tests.rs
+++ src/serialization/tests.rs
@@ -2,6 +2,70 @@ extern crate rand;
 
 use super::*;
 use self::rand::Rng;
+use std::io::Cursor;
+
+#[test]
+fn varint_write_3_bit_value() {
+    let buf = &mut Cursor::new(Vec::<u8>::new());
+    assert_eq!(1, super::varint_write(6, buf).unwrap());
+
+    let vec = buf.get_ref();
+    assert_eq!(1, vec.len());
+    assert_eq!(0x6, vec[0]);
+}
+
+#[test]
+fn varint_write_7_bit_value() {
+    let buf = &mut Cursor::new(Vec::<u8>::new());
+    assert_eq!(1, super::varint_write(127, buf).unwrap());
+
+    let vec = buf.get_ref();
+    assert_eq!(1, vec.len());
+    assert_eq!(0x7F, vec[0]);
+}
+
+#[test]
+fn varint_write_9_bit_value() {
+    let buf = &mut Cursor::new(Vec::<u8>::new());
+    assert_eq!(2, super::varint_write(256, buf).unwrap());
+
+    // marker high bit w/ 0's, then 9th bit (2nd bit of 2nd 7-bit group)
+    assert_eq!(&vec![0x80, 0x02], buf.get_ref());
+}
+
+#[test]
+fn varint_write_u64_max() {
+    let buf = &mut Cursor::new(Vec::<u8>::new());
+    assert_eq!(9, super::varint_write(u64::max_value(), buf).unwrap());
+
+    assert_eq!(&vec![0xFF; 9], buf.get_ref());
+}
+
+#[test]
+fn varint_read_u64_max() {
+    let input = &mut Cursor::new(vec![0xFF; 9]);
+    assert_eq!(u64::max_value(), super::varint_read(input).unwrap());
+}
+
+#[test]
+fn varint_read_u64_zero() {
+    let input = &mut Cursor::new(vec![0x00; 9]);
+    assert_eq!(0, super::varint_read(input).unwrap());
+}
+
+#[test]
+fn varint_write_read_roundtrip_rand() {
+    let mut rng = rand::weak_rng();
+    let mut vec = Vec::<u8>::new();
+    vec.reserve(9);
+    for _ in 1..1_000_000 {
+        vec.clear();
+        let int: u64 = rng.gen();
+        let bytes_written = super::varint_write(int, &mut vec).unwrap();
+        assert_eq!(vec.len(), bytes_written);
+        assert_eq!(int, super::varint_read(&mut vec.as_slice()).unwrap());
+    }
+}
 
 #[test]
 fn zig_zag_encode_0() {
@@ -42,8 +106,8 @@ fn zig_zag_decode_i64_max() {
 fn zig_zag_roundtrip_random() {
     let mut rng = rand::weak_rng();
 
-    for _ in 0..1_000_000{
-        let r = rng.gen::<i64>();
+    for _ in 0..1_000_000 {
+        let r: i64 = rng.gen();
         let encoded = zig_zag_encode(r);
         let decoded = zig_zag_decode(encoded);
 
