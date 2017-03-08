d43db6c8ced44cb9b13c2cef06f4698c72663b11
Marshall Pierce <marshall+github@truevault.com>
2017-03-08 09:50:27 -0800

    Better serialization benchmarks at different non-zero count densities

--- benches/serialization.rs
+++ benches/serialization.rs
@@ -11,33 +11,63 @@ use self::rand::distributions::IndependentSample;
 use self::test::Bencher;
 
 #[bench]
-fn serialize_small(b: &mut Bencher) {
+fn serialize_tiny_dense(b: &mut Bencher) {
+    // 256 + 3 * 128 = 640 counts
+    do_serialize_bench(b, 1, 2047, 2, 640 * 3 / 2)
+}
+
+#[bench]
+fn serialize_tiny_sparse(b: &mut Bencher) {
+    // 256 + 3 * 128 = 640 counts
+    do_serialize_bench(b, 1, 2047, 2, 640 / 10)
+}
+
+#[bench]
+fn serialize_small_dense(b: &mut Bencher) {
     // 2048 counts
-    do_serialize_bench(b, 1, 2047, 3)
+    do_serialize_bench(b, 1, 2047, 3, 2048 * 3 / 2)
 }
 
 #[bench]
-fn serialize_medium(b: &mut Bencher) {
+fn serialize_small_sparse(b: &mut Bencher) {
+    // 2048 counts
+    do_serialize_bench(b, 1, 2047, 3, 2048 / 10)
+}
+
+#[bench]
+fn serialize_medium_dense(b: &mut Bencher) {
     // 56320 counts
-    do_serialize_bench(b, 1, u64::max_value(), 3)
+    do_serialize_bench(b, 1, u64::max_value(), 3, 56320 * 3 / 2)
 }
 
 #[bench]
-fn serialize_large(b: &mut Bencher) {
-    // About 6 * 10^6 counts
-    do_serialize_bench(b, 1, u64::max_value(), 5)
+fn serialize_medium_sparse(b: &mut Bencher) {
+    // 56320 counts
+    do_serialize_bench(b, 1, u64::max_value(), 3, 56320 / 10)
 }
 
-fn do_serialize_bench(b: &mut Bencher, low: u64, high: u64, digits: u8) {
+#[bench]
+fn serialize_large_dense(b: &mut Bencher) {
+    // 6291456 buckets
+    do_serialize_bench(b, 1, u64::max_value(), 5, 6291456 * 3 / 2)
+}
+
+#[bench]
+fn serialize_large_sparse(b: &mut Bencher) {
+    // 6291456 buckets
+    do_serialize_bench(b, 1, u64::max_value(), 5, 6291456 / 10)
+}
+
+fn do_serialize_bench(b: &mut Bencher, low: u64, high: u64, digits: u8, random_counts: usize) {
     let mut s = V2Serializer::new();
-    let mut vec = Vec::new();
+    let mut vec = Vec::with_capacity(random_counts);
 
     let range = Range::new(low, high);
 
     let mut h = Histogram::<u64>::new_with_bounds(low, high, digits).unwrap();
 
     let mut rng = rand::weak_rng();
-    for _ in 0..100 {
+    for _ in 0..random_counts {
         h.record(range.ind_sample(&mut rng)).unwrap();
     };
 
@@ -46,6 +76,4 @@ fn do_serialize_bench(b: &mut Bencher, low: u64, high: u64, digits: u8) {
 
         let _ = s.serialize(&h, &mut vec).unwrap();
     });
-
-    b.bytes = vec.len() as u64;
 }
