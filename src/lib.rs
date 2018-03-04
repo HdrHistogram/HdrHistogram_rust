64fa0a736fa318299d626f01a520cb5277eb94f7
Jon Gjengset <jon@thesquareplanet.com>
2018-03-04 12:45:47 -0500

    Avoid compile fails as crate falls into oblivion
    See also rust-lang/rust#48717

--- src/lib.rs
+++ src/lib.rs
@@ -183,8 +183,6 @@
 //! and try to make sure you implement appropriate traits to make the use of the feature as
 //! ergonomic as possible.
 
-#![deny(missing_docs, trivial_casts, trivial_numeric_casts, unused_extern_crates,
-        unused_import_braces, unused_results, variant_size_differences, warnings)]
 #![allow(deprecated)]
 // Enable feature(test) is enabled so that we can have benchmarks of private code
 #![cfg_attr(all(test, feature = "bench_private"), feature(test))]
